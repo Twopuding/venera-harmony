c5083e281238037ad007658ee22c94fbd5850b2d
Twopudding <halole40@hotmail.com>
2026-06-28 02:16:56 +0800

    fix: Remove duplicate import in settings_page.dart
    修复 settings_page.dart 中重复导入 ohos_platform_services.dart 的问题
    
    Fix duplicate import of ohos_platform_services.dart in settings_page.dart

--- apps/app_ohos/lib/pages/settings/settings_page.dart
+++ apps/app_ohos/lib/pages/settings/settings_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_reorderable_grid_view/widgets/reorderable_builder.dart';
 import 'package:venera/platform/ohos_platform_services.dart';
-import 'package:venera/platform/ohos_platform_services.dart';
 import 'package:venera/components/components.dart';
 import 'package:venera/foundation/app.dart';
 import 'package:venera/foundation/appdata.dart';
